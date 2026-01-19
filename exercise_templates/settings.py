61706d8f5046532105ecdf66cbb27aa221c337c2
Johnyloco <alexmail095@gmail.com>
2026-01-19 09:06:55 +0100

    Add data to settings and install psycopg2 and freeze it

--- exercise_templates/settings.py
+++ exercise_templates/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'exercise_templates.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "urls_exercise_db",
+        "USER": "postgres-user",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
